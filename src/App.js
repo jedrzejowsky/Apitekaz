fb897fa7efedf8ce97e1cb1b50146a6f42cf7f64
jedrzejowsky <46368935+jedrzejowsky@users.noreply.github.com>
2023-11-23 01:21:48 +0100

    added navbar + darkmode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { useEffect, useState } from "react";
-import { CircularProgress } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  CircularProgress,
+  IconButton,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { auth } from "./config/firebase";
 import routes from "./config/routes";
 import Center from "./components/utils/Center";
 import AuthChecker from "./components/auth/AuthChecker";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -20,6 +31,26 @@ function App() {
     });
   }, []);
 
+  const toggleDarkMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    localStorage.setItem("darkMode", JSON.stringify(newMode));
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
   if (loading)
     return (
       <Center>
@@ -28,27 +59,36 @@ function App() {
     );
 
   return (
-    <div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                route.protected ? (
-                  <AuthChecker>
+    <ThemeProvider theme={theme}>
+      <div>
+        <IconButton
+          sx={{ position: "fixed", top: 10, right: 10, zIndex: 1000 }}
+          color="inherit"
+          onClick={toggleDarkMode}
+        >
+          {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Routes>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  route.protected ? (
+                    <AuthChecker>
+                      <route.component />
+                    </AuthChecker>
+                  ) : (
                     <route.component />
-                  </AuthChecker>
-                ) : (
-                  <route.component />
-                )
-              }
-            />
-          ))}
-        </Routes>
-      </BrowserRouter>
-    </div>
+                  )
+                }
+              />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
