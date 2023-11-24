2f0eff2d62e8e2711653163125a1675e7f86b28a
jedrzejowsky <46368935+jedrzejowsky@users.noreply.github.com>
2023-11-24 03:57:08 +0100

    code cleanup

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import Logout from "../components/auth/Logout";
 import Center from "../components/utils/Center";
+import MapPage from "../components/map/MapPage";
 
 const Home = (props) => {
   useEffect(() => {}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Center>
-      <Logout />
+      <MapPage />
     </Center>
   );
 };
