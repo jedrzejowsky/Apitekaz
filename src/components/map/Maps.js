ac53978f4a76bbd7adc4e2cc98bd8b4de0a7ff5a
jedrzejowsky <46368935+jedrzejowsky@users.noreply.github.com>
2023-11-28 15:34:18 +0100

    waypoints integration with firestore

--- src/components/map/Maps.js
+++ src/components/map/Maps.js
@@ -1,75 +1,81 @@
-import { divIcon } from 'leaflet';
-import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-import MarkerClusterGroup from 'react-leaflet-cluster';
-import data from './geoPharmacies_0_800.json';
-import L, { MarkerCluster } from "leaflet";
-import  url  from './placeholder.png';
-
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Firebase } from "../../config/firebase"; // Import Firebase z Twojego pliku
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import L from "leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import url from "../../assets/placeholder.png";
 
 export default function Maps(props) {
+  const [pharmacies, setPharmacies] = useState([]);
   const { selectPosition } = props;
-  const locationSelection = [selectPosition?.lat, selectPosition?.lon];
   const position = [51.919438, 19.145136];
-  
-  const customIcon =  new L.Icon({
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const db = getFirestore(Firebase); // Uzyskaj dostęp do Firestore z Firebase
+
+        // Pobranie danych z bazy Firestore
+        const pharmaciesCollection = collection(db, "pharmacy"); // Nazwa kolekcji w Firestore
+        const snapshot = await getDocs(pharmaciesCollection);
+        const pharmaciesData = [];
+
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          pharmaciesData.push({
+            id: doc.id,
+            ...data,
+          });
+        });
+
+        setPharmacies(pharmaciesData);
+      } catch (error) {
+        console.error("Error fetching pharmacies:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const customIcon = new L.Icon({
     iconUrl: url,
     iconRetinaUrl: url,
     iconSize: new L.Point(40, 47),
-    
-  })
-
-  const createClusterCustomIcon = function (cluster) {
-    return L.divIcon({
-      html: `<span>${cluster.getChildCount()}</span>`,
-      className: "custom-marker-cluster",
-      iconSize: L.point(33, 33, true)
-    });
-  };
+  });
 
   return (
     <div style={{ width: "50vw", height: "100vh" }}>
-      <MapContainer center={position} zoom={6} scrollWheelZoom={true}  style={{ border: "2px solid red", width: "100%", height: "100%" }} >
+      <MapContainer
+        center={position}
+        zoom={6}
+        scrollWheelZoom={true}
+        style={{ border: "2px solid red", width: "100%", height: "100%" }}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        
-        <MarkerClusterGroup
-        chunkedLoading
-      >
-        {data.map((address, index) => (
-          <Marker
-            key={index}
-            position={[address.position.lat, address.position.lng]}
-            title={address.name}
-            icon= {customIcon}
-            
-          >
-            <Popup>
-              <div>
-                  <p>{address.name}</p>
-                  <p>{address.address.label}</p>
-                  <p>tel. {address.phoneNumber}</p>
-              </div>
-            </Popup>
-          </Marker>
-          
-        ))}
-      </MarkerClusterGroup>
 
-
-        {/* <Marker position={position}>
-          <Popup>
-            
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker> */}
-        {/* <Marker position={[51.819438, 19.155136]}/> */}
-
-
+        <MarkerClusterGroup chunkedLoading>
+          {pharmacies.map((pharmacy, index) => (
+            <Marker
+              key={index}
+              position={[pharmacy.position.lat, pharmacy.position.lng]}
+              title={pharmacy.name}
+              icon={customIcon}
+            >
+              <Popup>
+                <div>
+                  <p>{pharmacy.name}</p>
+                  <p>{pharmacy.address.label}</p>
+                  <p>tel. {pharmacy.phoneNumber}</p>
+                </div>
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
       </MapContainer>
     </div>
-
-  )
+  );
 }
