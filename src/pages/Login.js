9a0072340b706ec53a2b5f10100de513b13e723b
Chris <kbalicki2014@gmail.com>
2024-01-11 00:59:12 +0100

    fix back to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,14 @@
 import { Box, Tab, Tabs } from "@mui/material";
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import {useEffect} from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import AuthContainer from "../components/auth/AuthContainer";
 import Center from "../components/utils/Center";
 import LoginForm from "../components/forms/loginForm";
 import RegistrationForm from "../components/forms/registrationForm";
 
 import backgroundImage from "../assets/background1.png";
+import {getAuth, onAuthStateChanged } from "firebase/auth";
 
 const tabIdToURL = {
   0: "login",
@@ -15,6 +17,8 @@ const tabIdToURL = {
 
 const Login = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate(); // Użyj useNavigate do uzyskania funkcji navigate
+  const auth = getAuth(); 
 
   const action = searchParams.get("action") || "login";
   let indexFromUrl = 0;
@@ -29,6 +33,17 @@ const Login = () => {
     const action = tabIdToURL[newValue];
     setSearchParams({ action });
   };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/map", { replace: true });
+      }
+    });
+    return () => unsubscribe();
+    // Anuluj subskrypcję, gdy komponent jest niepotrzebny
+    
+  }, [auth, navigate]);
 
   return (
     <Center height={90}>
