53c1030e9d2bbd39678ca8f37703595d72bc319c
jedrzejowsky <46368935+jedrzejowsky@users.noreply.github.com>
2024-01-11 01:10:21 +0100

    Update AuthContainer.js

--- src/components/auth/AuthContainer.js
+++ src/components/auth/AuthContainer.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth, Providers } from "../../config/firebase";
-import { doc, setDoc, getDoc } from "firebase/firestore"; // Dodaj te importy
 import { Button, Typography } from "@mui/material";
 import GoogleIcon from "@mui/icons-material/Google";
 import Center from "../utils/Center";
@@ -12,45 +11,13 @@ const AuthContainer = (props) => {
   const [errorMessage, setErrorMessage] = useState("");
   const [disabled, setDisabled] = useState(false);
 
-  const handleRegistration = async (email, password) => {
-    // Dodaj parametry email i hasło
-    try {
-      setDisabled(true);
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      setDisabled(false);
-      console.info("TODO: Navigate to authenticated screen after registration");
-      const user = userCredential.user;
-      const userRef = doc(auth, "userLikedPharmacy", userCredential.user.uid);
-      const createLikedDoc = await getDoc(userRef);
-      if (!createLikedDoc.exists()) {
-        try {
-          await setDoc(userRef, {
-            list: [],
-            user: userCredential.user.uid,
-          });
-        } catch (error) {
-          console.error("Error while creating user document", error);
-        }
-      }
-      console.log("User registered:", user);
-      navigate("/map", { replace: true });
-    } catch (error) {
-      setErrorMessage(error.code + ": " + error.message);
-      setDisabled(false);
-    }
-  };
-
   const signInWithGoogle = () => {
     setDisabled(true);
     signInWithPopup(auth, Providers.google)
       .then(() => {
         setDisabled(false);
         console.info("TODO: navigate to authenticated screen");
-        navigate("/map", { replace: true });
+        navigate("/");
       })
       .catch((error) => {
         setErrorMessage(error.code + ": " + error.message);
@@ -67,7 +34,7 @@ const AuthContainer = (props) => {
         variant="contained"
         onClick={signInWithGoogle}
       >
-        Kontynuuj przez konto Google{" "}
+        Kontynuuj przez konto google{" "}
       </Button>
       <Typography sx={{ mt: 2 }} color={"red"}>
         {errorMessage}
